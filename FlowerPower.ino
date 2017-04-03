a96a9d3c74b67dd96a15a3b70d8705fc6bb26db0
enordstrand <eiriknordstrand@gmail.com>
2017-04-03 07:51:36 +0200

    Changed the pinout and added motor

--- FlowerPower.ino
+++ FlowerPower.ino
@@ -1,11 +1,17 @@
 // YL-39 + YL-69 humidity sensor
 byte humidity_sensor_pin = A1;
-byte humidity_sensor_vcc = 6;
+byte humidity_sensor_vcc = 19;
+byte humidity_sensor_gnd = 17;
+byte motor_pin = 3;
 
 void setup() {
   // Init the humidity sensor board
   pinMode(humidity_sensor_vcc, OUTPUT);
-  digitalWrite(humidity_sensor_vcc, LOW);
+  pinMode(humidity_sensor_gnd, OUTPUT);
+  pinMode(motor_pin, OUTPUT);
+  digitalWrite(humidity_sensor_vcc, HIGH);
+  digitalWrite(humidity_sensor_gnd, LOW);
+  digitalWrite(motor_pin, LOW);
 
   // Setup Serial
   while (!Serial);
@@ -23,6 +29,12 @@ int read_humidity_sensor() {
 
 void loop() {
   Serial.print("Humidity Level (0-1023): ");
-  Serial.println(read_humidity_sensor()); 
-  delay(10000);
+  Serial.println(read_humidity_sensor());
+  delay(1000);
+  digitalWrite(motor_pin, HIGH);
+  digitalWrite(13, HIGH);
+  delay(5000);
+  digitalWrite(motor_pin, LOW);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
