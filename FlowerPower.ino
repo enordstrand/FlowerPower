da01c48d98ea6d272f69c8078544f82e156842fe
enordstrand <eiriknordstrand@gmail.com>
2017-04-08 10:43:11 +0200

    Added Pushbutton test

--- FlowerPower.ino
+++ FlowerPower.ino
@@ -4,8 +4,12 @@
 static const int humidity_sensor_pin = A1;
 static const int humidity_sensor_vcc = 19;
 static const int humidity_sensor_gnd = 17;
+static const int btn_pin = 12;
+static const int btn_gnd = 10;
 static const int relayPin1 = 3;
+static const int test_led = 13;
 static const long windowSize = 60000;
+
 
 double setpoint1, input1, output1;
 double kp=2000, ki=0, kd=0;
@@ -24,11 +28,16 @@ void setup() {
     pinMode(humidity_sensor_pin, INPUT);
     pinMode(humidity_sensor_vcc, OUTPUT);
     pinMode(humidity_sensor_gnd, OUTPUT);
+    pinMode(test_led, OUTPUT);
     pinMode(relayPin1, OUTPUT);
+    pinMode(btn_pin, INPUT_PULLUP);
+    pinMode(btn_gnd, OUTPUT);
 
-    digitalWrite(humidity_sensor_vcc, HIGH);
+    digitalWrite(humidity_sensor_vcc, LOW);
     digitalWrite(humidity_sensor_gnd, LOW);
     digitalWrite(relayPin1, LOW);
+    digitalWrite(btn_gnd, LOW);
+    digitalWrite(test_led, LOW);
 
     //input1 = analogRead(0);
     myPID1.SetMode(AUTOMATIC);
@@ -49,15 +58,26 @@ void setup() {
 void loop() {
     uint8_t i;
     float averageAnalogRead_1;
+
+    //read the pushbutton value into a variable
+    int sensorVal = digitalRead(btn_pin);
+    //print out the value of the pushbutton
+    Serial.println(sensorVal);
+
+    if (sensorVal == HIGH) {
+        digitalWrite(test_led, LOW);
+    } else {
+        digitalWrite(test_led, HIGH);
+    }
    
     // take N samples in a row, with a slight delay
-    digitalWrite(humidity_sensor_vcc, HIGH);
-    delay(500);
+    //digitalWrite(humidity_sensor_vcc, HIGH);
+    //delay(500);
     for (i=0; i< numberOfSamples; i++) {
-     samples1[i] = read_humidity_sensor();
+       samples1[i] = read_humidity_sensor();
 //     delay(10);
     }
-    digitalWrite(humidity_sensor_vcc, LOW);
+    //digitalWrite(humidity_sensor_vcc, LOW);
    
     // average all the samples out
     averageAnalogRead_1 = 0;
@@ -79,6 +99,9 @@ void doPid() {
         windowStartTime += windowSize;
     }
 
+    if (output1 > 3000) {
+      output1 = 3000;
+    }
   
     if ((output1 > now - windowStartTime) && output1 > 100 && humidity1 > -1) {
         digitalWrite(relayPin1, HIGH);
@@ -90,16 +113,19 @@ void doPid() {
         Serial.print("L1 ");
     }
 
-    Serial.print("H1: ");
+    Serial.print("Hum1: ");
     Serial.print(humidity1);
     Serial.print(" W1: ");
     Serial.print(output1);
-    Serial.print(" T: ");
+    Serial.print(" T1: ");
+    Serial.print(now);
+    Serial.print(" WT: ");
     Serial.println(now - windowStartTime);
 }
 
 int read_humidity_sensor() {
   int value = analogRead(humidity_sensor_pin);
-  return 1023 - value;
+  //return 1023 - value;
+  return 123;
 }
 
